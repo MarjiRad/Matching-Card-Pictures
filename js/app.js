c63fa18a7dd6a2c191c4acc804984cc2b2530e79
Marjan Rad <marjan.n.rad@gmail.com>
2025-05-13 19:22:53 -0400

    Matching Card Picture

--- js/app.js
+++ js/app.js
@@ -3,19 +3,29 @@
 
 
 //store id="livesCount" in variable livesCount
-const livescount = document.getElementById("livesCount");
+const livesCount = document.getElementById("livesCount");
 
 //store id="scoreCount" in variable score
-const score = document.getElementById("scoreCount");
+const scoreCount = document.getElementById("scoreCount");
 
 //store id="time" in variable timeCount
-const timeCountDown = document.getElementById("timeCount");
+const timeCount = document.getElementById("timeCount");
 
 //store id="game-board" in variable gameBoard
 const gameBoard = document.getElementById("game-board");
 
-//store id="restart" in variable restartButton
-const restartButton = document.getElementById("restart");
+//store all button id="restart" in variable restartButtons
+const restartButtons = document.querySelectorAll(".restart");
+
+//store .overlay-text in variable overlayText
+const overlayText = document.querySelector(".overlay-text");
+
+//store input in variable input
+const input = document.querySelector('input');
+
+//store playerNameDisplay in variable playerName
+const playerName = document.getElementById('playerNameDisplay');
+
 
 //creat array of all suits cards with their keys
 const cardValues = {
@@ -37,25 +47,27 @@ const cardValues = {
 };
 console.log(cardValues);
 
+
 const suits = ["heart", "club", "diamond", "spade"];
 console.log(suits);
+
 
 const cardNames = Object.keys(cardValues);
 console.log(cardNames);
 
+//object of 4 array of each suit------------------------------------------------
 const allCards = {};
-suits.map(suit => {
-        allCards[suit] = cardNames.map(name =>{
-                return {
+suits.forEach(suit => {
+        allCards[suit] = cardNames.map(name => ({
                         value: cardValues[name],
                         name: name,
                         src: `images/${suit}/${name}.jpg`
-                };
-        });
+        }));
 });
 console.log(allCards);
 
-//shuffle function
+
+//shuffle function for array----------------------------------------------------
 function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--){
                 const j = Math.floor(Math.random() * (i + 1));
@@ -63,285 +75,181 @@ function shuffle(array) {
         }
         return array;
 }
-
-const suitButtons = document.querySelectorAll("#buttons button");
-console.log(suitButtons);
  
 
-const cardBacks = document.querySelectorAll(".card-back");
-
-
-
-//function to handle suit selection
-suitButtons.forEach(button => {
-        button.addEventListener('click', () => {
-                const suit =button.id;//'heart', 'club', 'diamond', 'spade'
-                const selectedCards = allCards[suit];
-                const duplicatedCards = [...selectedCards, ...selectedCards]; 
-                const shuffledCards = shuffle(duplicatedCards);
-                const cardFrontImages = document.querySelectorAll('.card-front .name');
-                cardFrontImages.forEach((img,index) => {
-                   img.src = shuffledCards[index].src;
-                cardBacks[index].addEventListener ("click", (event) => {
-                        img.style.display = "flex";
-                        console.log(img);
-                        console.log("test");
-                        cardBacks[index].style.display = "none"
-                        
-                });
-                        
-        
-});
-                console.log(suit);
-                console.log(selectedCards);
-                console.log(duplicatedCards);
-        });
-});
-
-
-
-
-/*
-function createBoard() {
-    //clear the current board
-    board.innerHtml ="";
-    //clear the matched pairs
-    matchedPairs = 0;
-     //reset lives to 6
-    lives = 6;
-    //update  the display value of the lives left on the screen by settingin the current lives value
-    lives.textContent = 'lives';
-    cards.forEach((imgSrc, index) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.dataset.index = index;
-
-        const img = document.createElement("img");
-        img.src = `images/${imgSrc}`;
-        img.atl = "cat Image";
-
-        card.appendChild(img);
-        board.appendChild(card);
-
-        card.addEventListener("click", flipCard);
-     });
-}
-
-
+//game state---------------------------------------------------------------------
 // declears variable flippedCard
 let flippedCards =[];
 // get valiue of matchPairs = 0
 let matchedPairs = 0;
 //set the number of lives 6
-let lives = 6;
+let lives = 10;
 //set the time on 100
 let time = 100;
+//set the timer start to countdown
+let countdownInterval;
+let timerStarted = false;
+let boardLocked = false;
 
 
 
-
-
-
-
-
-
-function flipCard() {
-    if(flippedCards.length<2 && !this.classList.contains("flipped")){
-        this.classList.add("fliopped");
-        flippedCards.push(this);
-
-        if(flippedCards.length === 2) {
-            setTimeout(checkMatch, 700);
-        }
-    }
-}
-
-
-
-function checkMatch() {
-    const [card1, card2] = flippedCards;
-    const img1 = card1.querySelector("img").src;
-    const img2 = card2.querySelector("img").src;
-    if(img1 === img2) {
-        matchedPairs++;
-        flippedCards = [];
-
-        if(matchedPairs === images.length) {
-          setTimeout(() => alert("You Win!"), 300);
-        }
-    }else{
-        lives--;
-        liveseCount.textContent = lives;
-         setTimeout(() => {
-            card1.classList.remove("flipped");
-            card2.classList.remove("flipped");
-        }, 700);
-        
-        if(lives === 0) {
-            setTimeout(() => alert("Game Over!"), 300);
-        }
-    }
+//start or reset game-------------------------------------------------------------
+function startGame() {
+    //clear the current board
+    gameBoard.innerHTML ="";
+    //clear the matched pairs
+    matchedPairs = 0;
+    //reset lives to 6
+    lives = 10;
+    //reset time to 100
+    time = 100;
+    //reset fliped card
     flippedCards = [];
-}
+    // start timer count down
+    timerStarted = false;
 
-
-restartButton.addEventListener("click", () => {
-    cards.sort(() => 0.5 - Math.random());
-    createBoard();
-});
-
-createBoard();
-
-
-
-
-
-
-
-
-
-
-function readyToStart() {
-        let overlays = Array.from(document.getElementsByClassName('overlay-text'));//make an array of overlay-text
-        overlays.forEach(overlay => {
-                overlay.addEventListener('click', () => {
-                        overlay.classList.remove('visible');
-                        //game.startGame();
-                });
-        });
-        cards.forEach(card => {
-                card.addEventListener('click', () => {
-                        //game.flipCard(card);
-                });
-        });
-}
-
-
-;
-
-
-
-
-function ready() {
-        let overlays = Array.from(document.getElementsByClassName('overlay-text'));//make an array of overlay-text
-        let cards = Array.from(document.getElementsByClassName('card'));//make an array of card
-
-        overlays.forEach(overlay => {
-                overlay.addEventListener('click', () => {
-                        overlay.classList.remove('visible');
-                        //game.startGame();
-                });
-        });
-        cards.forEach(card => {
-                card.addEventListener('click', () => {
-                        //game.flipCard(card);
-                });
-        });
+    livesCount.textContent = lives;
+    scoreCount.textContent = 0;
+    timeCount.textContent = time;
+    playerName.textContent = `Player: ${input.value.trim()}`;
+    document.querySelector(".overlay-text").classList.remove("visible");
 };
 
 
-const gameBoard = document.getElementById("game-board");
-console.log(gameBoard);
+//start countdown------------------------------------------------------------------
+function startCountdown() {
+        clearInterval(countdownInterval);
+        countdownInterval = setInterval(() => {
+         time--;
+         timeCount.textContent = time;
 
-
-
-
-
-//function to handle suit selection
-suitButtons.forEach(button => {
-        button.addEventListener('click', () => {
-                const suit =button.id;//'heart', 'club', 'diamond', 'spade'
-                console.log(suit);
-        });
-});
-
-//const cardNames = [
-        "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "joker", "scrap"
-]
-
-function createShuffleDeck(suit) {
-        //create 15 cards with file names
-        const cards = cardNames.map(name => `${name}.jpg`);
-        //duplicate cards
-        const doubledCards = [...cards, ...cards];
-        //shuffle
-        for (let i = doubledCards.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [doubledCards[i], doubledCards[j]] = [doubledCards[j], doubledCards[i]];
-        }
-        return doubledCards.map(fileName => ({
-                imgSrc: `images/${suit}/${fileName}`,
-                name: fileName.split('.')[0]
-        }))
+         if (time <= 0) {
+                clearInterval(countdownInterval);
+                document.getElementById("game-over-text").classList.add("visible");
+         }
+        }, 1000);
 }
 
+
+//load cards into the game board---------------------------------------------------
 function loadBoard(deck) {
-        gameBoard.innerHTML = ""; //clear the board
+        gameBoard.innerHTML = "";
+
         deck.forEach(card => {
-                const cardElement = document.createElement('div');
-                cardElement.classList.add('card');
-                cardElement.innerHTML =`
-                  <div class="card-back card-face">
-                     <img class="backside" src="images/back/back-face.png"/>
-                  </div>
-                  <div class="card-front card-face">
-                     <img class="card-value" src="${card.imgSrc}" data-name="${card.name}"/> 
-                  </div>`;
+             const cardElement = document.createElement('div');
+             cardElement.classList.add('card');
+
+             cardElement.innerHTML = `
+                 <div class="card-back card-face">
+                   <img class="backside" src="images/back/back-face.png"/>
+                </div>
+                <div class="card-front card-face">
+                  <img class="card-value" src="${card.imgSrc}"/> 
+                </div>`;
+
+                cardElement.addEventListener('click', flipCard);
                 gameBoard.appendChild(cardElement);
         });
 }
 
 
+//flip cards rules----------------------------------------------------------------
+function flipCard(event) {
+        if (boardLocked) return;
 
-//
+        if (!timerStarted) {
+                startCountdown();
+                        timerStarted = true;
+        };
+        const card = event.currentTarget;
+        if(flippedCards.length < 2 && !card.classList.contains("flipped")) {
+           card.classList.add("flipped");
+           flippedCards.push(card);
+           if(flippedCards.length === 2) {
+              boardLocked = true;
+              setTimeout(() => {
+                checkMatch();
+                boardLocked = false;
+              }, 700);
+           }
+        }
+}
 
-/*function setupBoard(suit) {
-        //clear board before setup
-        gameBoard.innerHTML = ''
-        //hide all suits
-        document.querySelectorAll('#Heart, #club, #spade, #diamond').forEach(el => el.computedStyleMap.display = 'none');
-        //get image sources from the selected suit
-        const suitDiv = document.getElementById(capitalize(suit));
-        console.log(suitDiv);
-        const cardImages = Array.from(suitDiv.querySelectorAll('img')).map(img => img.src);
-        //select 15 unique images and duplicate
-        constSelectedCards = shuffle(cardImages).slice(0, 15);
-        const gameCards = shuffle([...selectedCards, ...selectedCards]); //30 cards in total
 
-        //add cards to the game board
-        gameCards.forEach(src => {
-                const card = document.createElement('div');
-                card.classList.add('card');
-
-                const front = document.createElement('img');
-                front.scr = scr;
-                front.classicList.add('front');
-
-                const back = document.createElement('img');
-                back.scr = document.querySelector('#backsideCard img').src;
-                card.classList.add('back');
-                card.appendChild(front);
-                card.appendChild(back);
-                card.addEventListener('click', () => flipCard(card));
-                gameBoard.appendChild(card);
-        });
+//match logic---------------------------------------------------------------------
+function checkMatch() {
+    const [card1, card2] = flippedCards;
+    const img1 = card1.querySelector(".card-value").src;
+    const img2 = card2.querySelector(".card-value").src;
+    if(img1 === img2) {
+        matchedPairs++;
+        flippedCards = [];
+        const matchedValue = cardValues[card1.querySelector(".card-value").src.split('/').pop().split(".")[0]]
+        scoreCount.textContent = parseInt(scoreCount.textContent) + matchedValue; 
+        if(matchedPairs === 15) {
+                document.getElementById("victory-text").classList.add("visible");
+        }
+    }else{
+        lives--;
+        livesCount.textContent = lives;
+        setTimeout(() => {
+            card1.classList.remove("flipped");
+            card2.classList.remove("flipped");
+        }, 700);
+        if(lives === 0) {
+                document.getElementById("game-over-text").classList.add("visible");   
+        }
+        flippedCards = [];
+    };
 };
 
-function shuffle(array) {
-        return array.sort(() => Math.random() - 0.5);
-};
 
-functionCapitalize(str) {
-        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-}*/
+//suit button logic----------------------------------------------------------------
+const suitButtons = document.querySelectorAll("#buttons button");
+console.log(suitButtons);
+suitButtons.forEach(button => {
+        button.addEventListener('click', () => {
+                if (!input.value.trim()) {
+                        alert("please enter your name");
+                        return;
+                }
+                const suit = button.id;//'heart', 'club', 'diamond', 'spade'
+                const selectedCards = allCards[suit].slice(0, 15);
+                const duplicatedCards = [...selectedCards, ...selectedCards]; 
+                const shuffledDeck = shuffle(duplicatedCards.map(card => ({
+                        imgSrc: card.src,
+                        name: card.name
+                })));
+
+                console.log("Selected:", selectedCards);
+                console.log("duplicated:", duplicatedCards);
+                console.log("shuffled:", shuffledDeck);
+
+                startGame();
+                loadBoard(shuffledDeck);
+        });       
+});
 
 
+//restart buttons logic--------------------------------------------------------------
+restartButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+                document.getElementById("victory-text").classList.remove("visible");
+                document.getElementById("game-over-text").classList.remove("visible");
+                gameBoard.innerHTML = "";
+                input.style.display = "inline";
+                playerName.textContent = "";
+                overlayText.classList.add("visible");
+                clearInterval(countdownInterval);
+                time = 100;
+                timeCount.textContent = time;
+                timerStarted = false;
+        });   
+});   
 
 
-/*-------------------------------- Variables --------------------------------*/
-
-/*------------------------ Cached Element References ------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
+//start overlay click
+overlayText.addEventListener("click", () => {
+        overlayText.classList.remove("visible");
+});
+ 
